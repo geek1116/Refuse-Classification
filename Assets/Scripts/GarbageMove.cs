3353ef7eb9e8e70c9b13c516b48109675c94d461
geek1116 <1035214549@qq.com>
2019-07-18 19:32:04 +0800

    #垃圾移动

---
+++ Assets/Scripts/GarbageMove.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GarbageMove : MonoBehaviour
+{
+    private List<Vector3> arrPath = LevelManager.arrPath;
+    private int curIndex = 0;
+    private int arrCount = 0;
+    private float speed = LevelManager.speed;
+    void Start()
+    {
+        arrCount = arrPath.Count;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(curIndex < arrCount)
+        {
+            if(transform.position != arrPath[curIndex])
+            {
+                Vector2 temp = Vector2.MoveTowards(transform.position, arrPath[curIndex], speed*Time.deltaTime);
+                GetComponent<Rigidbody2D>().MovePosition(temp);
+                Debug.Log(curIndex);
+            }
+            else
+            {
+                curIndex++;
+            }
+        }
+    }
+}
