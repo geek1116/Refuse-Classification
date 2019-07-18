eddb5c613f0352993b3b0e4739526697c0030753
zrous <45260300+zrous@users.noreply.github.com>
2019-07-18 10:55:31 +0800

    Create Garbage.cs

---
+++ Assets/Scripts/Garbage.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GarbageData
+{
+    public int code;
+    public string name;
+    public int type;
+    public int imageId;
+    Garbage(int _code, string _name, int _type, int _imageId){
+        code = _code;
+        name = _name;
+        type = _type;
+        imageId = _imageId;
+    }
+}
+
+public class Garbage : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
