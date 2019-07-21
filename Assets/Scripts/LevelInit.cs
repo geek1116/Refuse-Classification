a6687fc475cdb97438c1f51c24d0e546758dd140
geek1116 <1035214549@qq.com>
2019-07-21 13:35:38 +0800

    #垃圾桶预制体

---
+++ Assets/Scripts/LevelInit.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelInit : MonoBehaviour
+{
+    public GameObject trashcanPrefab;
+
+    private Map map;
+
+    private float trashcanX = -4.5f;
+
+    private float trashcanY = -7.5f;
+
+    private float interval = 3f;
+
+    private List<GameObject> trashcans = new List<GameObject>();
+
+    void OnEnable()
+    {
+        Debug.Log("kkkkk!!!");
+        map = GameData.config.GetMapConfig(1);
+        List<int> carType = map.GetCarType();
+        int[] temp = {1,1,1,1,1};
+        foreach (int item in carType) if(1<=item && item<=4) temp[item]=0;
+
+        Config config = GameData.config;
+        for(int i=1;i<=4;i++)
+        {
+            if(temp[i]==1)
+            {
+                GameObject trashcan = Instantiate(trashcanPrefab, new Vector3(trashcanX,trashcanY), Quaternion.identity);
+                trashcan.GetComponent<SpriteRenderer>().sprite = config.GetTrashCanImage(i);
+                trashcan.GetComponent<TrashCan>().SetType(i);
+                trashcanX += interval;
+                trashcans.Add(trashcan);
+            }
+        }
+    }
+
+    void OnDisable()
+    {
+        foreach (GameObject item in trashcans)
+        {
+            Destroy(item);
+        }
+    }
+
+}
