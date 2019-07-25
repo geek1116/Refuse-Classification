f6c8677a209aea951a1bf1af35899a36ba431a10
geek1116 <1035214549@qq.com>
2019-07-25 10:48:17 +0800

    #音效2

--- Assets/Scripts/Cat.cs
+++ Assets/Scripts/Cat.cs
@@ -4,15 +4,26 @@ using UnityEngine;
 
 public class Cat : MonoBehaviour
 {
+
+    public AudioClip pickCorrect;
+
+    public AudioClip pickWrong;
+
+    private AudioSource audioSource;
+
     private Animator animator;
 
     private void Awake()
     {
         animator = GetComponent<Animator>();
+        audioSource = GetComponent<AudioSource>();
     }
 
     public void CollectGarbage(bool isPickedRight)
     {
+        if(isPickedRight) audioSource.PlayOneShot(pickCorrect);
+        else audioSource.PlayOneShot(pickWrong);
+        
         animator.SetBool("PickedStatus", isPickedRight);
         animator.SetTrigger("Collect");
     }
